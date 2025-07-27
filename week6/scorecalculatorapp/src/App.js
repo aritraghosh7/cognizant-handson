8a6dbf4119f88cdd9533eb27f2d7c3eaa2bfcc5b
Aritra Ghosh <aritraghosh184@gmail.com>
2025-07-27 23:31:02 +0530

    Add React assignments in week6 folder

---
+++ week6/scorecalculatorapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CalculateScore from './Components/CalculateScore';
+
+function App() {
+  return (
+    <div className="App">
+      <CalculateScore name="Aritra Ghosh" school="KIIT" total={450} goal={5} />
+    </div>
+  );
+}
+
+export default App;
---
+++ week6/trainersapp/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Trainers App</h1>
+      <p>This is the home page where you can find information about trainers.</p>
+    </div>
+  );
+}
+
+export default Home;
